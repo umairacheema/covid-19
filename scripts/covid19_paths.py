3e04ffa57fd81b7fb5e74ded711f08791fb2022a
umairacheema <umair@hotmail.com.au>
2020-07-09 08:26:09 +0300

    Removed interactive_plots folder

--- scripts/covid19_paths.py
+++ scripts/covid19_paths.py
@@ -1,5 +1,5 @@
 DATA_DIR = '../csse_covid_19_data'
 PLOTS_DIR = '../docs/plots'
-INTERACTIVE_PLOTS_DIR = '../docs/interactive_plots'
+INTERACTIVE_PLOTS_DIR = '../docs/interactive-plots'
 PATH_TO_PLOTLY = '../assets/js/plotly.min.js'
 AGGREGATE = '../docs/_data/aggregate.json'

b7e6bf80d5e8c7621afabcfd877bfc5a5f41604f
umairacheema <umair@hotmail.com.au>
2020-07-10 18:56:46 +0300

    Turn off autoplay

--- scripts/covid19_plots.py
+++ scripts/covid19_plots.py
@@ -117,7 +117,7 @@ def plot_scatter_map(df,map_style='open-street-map',cases='Confirmed'):
     fig.write_html(os.path.join(cc.INTERACTIVE_PLOTS_DIR,'cases-'+ cases.lower()
                     + '-global.html'),include_plotlyjs = cc.PATH_TO_PLOTLY, auto_play=False)
     fig.write_html(os.path.join(cc.INCLUDES_DIR,'cases-'+ cases.lower() + '-global.html'),
-                    include_plotlyjs = False, full_html = False)
+                    include_plotlyjs = False, full_html = False, auto_play=False)
 
 
 def plot_continents(df, cases='Confirmed'):
